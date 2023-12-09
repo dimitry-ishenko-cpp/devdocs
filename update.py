93654feb54198672b6a6300a3e106f346362c664
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2023-12-08 23:34:16 -0500

    Refactor the update script

--- update.py
+++ update.py
@@ -2,34 +2,33 @@
 
 import json
 from pathlib import Path
-import urllib.request as request
-
-def spin():
-    while True:
-        for c in "||//--\\\\": yield c
-spinner = spin()
-
-def reporthook(*args):
-    print(next(spinner) + "\b", end="", flush=True)
+from urllib.request import build_opener, install_opener, Request, urlopen, urlretrieve
 
 def download(url, path, info):
+    def spin():
+        while True:
+            for c in "||//--\\\\": yield c
+    spinner = spin()
+
+    def reporthook(*args):
+        print(next(spinner) + "\b", end="", flush=True)
+
     print(f"{info:.<40s} ", end="")
-    request.urlretrieve(url, path, reporthook)
+    urlretrieve(url, path, reporthook)
     print("OK")
 
 root = Path("html")
 if not root.exists():
     raise RuntimeError("Must be run from with the root directory")
 
-opener = request.build_opener()
+opener = build_opener()
 opener.addheaders = [("User-agent", "Mozilla/5.0")]
-request.install_opener(opener)
+install_opener(opener)
 
-docs_name = "docs.json"
-docs_path = root / docs_name
-download("https://devdocs.io/docs.json", docs_path, f"Document index ({docs_name})")
+req = Request("https://devdocs.io/docs.json", headers={"User-agent": "Mozilla/5.0"})
+index = json.load(urlopen(req))
 
-for doc in json.load(docs_path.open()):
+for doc in index:
     doc_mtime = str(doc["mtime"])
     doc_slug = doc["slug"]
     doc_path = root / doc["type"] / doc_slug
@@ -41,8 +40,6 @@ for doc in json.load(docs_path.open()):
         doc_path.mkdir(parents=True, exist_ok=True)
 
         for doc_name in [ "index.json", "db.json" ]:
-            download(f"https://documents.devdocs.io/{doc_slug}/{doc_name}", doc_path / doc_name, f"{doc_slug} ({doc_name})") 
+            download(f"https://documents.devdocs.io/{doc_slug}/{doc_name}", doc_path / doc_name, f"{doc_slug} ({doc_name})")
 
         mtime_path.write_text(doc_mtime)
-
-print("DONE")
