5f63dc0f445aa6ed7a3fb2fa4fac5cd6a307ac10
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2023-12-09 22:04:24 -0500

    Refactor the update script

--- update.py
+++ update.py
@@ -4,16 +4,13 @@ import json
 from pathlib import Path
 import re
 import shutil
+from tempfile import TemporaryDirectory as tempdir
 import urllib.request as request
 
 docs_url = "https://devdocs.io/docs.json"
-doc_prefix_url = "https://documents.devdocs.io"
+doc_templ_url = "https://documents.devdocs.io/{}/{}"
 
-index_name = "index.json"
-db_name = "db.json"
-mtime_name = "mtime"
-
-def download1(url, path, info):
+def download(url, banner):
     def spin():
         while True:
             for c in "||//--\\\\": yield c
@@ -22,69 +19,65 @@ def download1(url, path, info):
     def reporthook(*args):
         print(next(spinner) + "\b", end="", flush=True)
 
-    print(f"{info:.<40s} ", end="")
-    request.urlretrieve(url, path, reporthook)
-    print("OK")
+    with tempdir() as dirname:
+        path = Path(dirname) / "file.json"
+        print(f"{banner:.<40s} ", end="")
+        request.urlretrieve(url, path, reporthook)
+        with open(path) as fp: content = json.load(fp)
+        print("OK")
 
-    return json.load(open(path))
+    return content
 
-def download2(path, name):
-    path.mkdir(parents=True, exist_ok=True)
-
-    index = download1(doc_prefix_url+"/"+name+"/"+index_name, path / index_name, name+" index")
-    db = download1(doc_prefix_url+"/"+name+"/"+db_name, path / db_name, name+" db")
-
-    return index, db
-
-def clean(path):
+def purge(path):
     for child in path.iterdir():
         shutil.rmtree(child) if child.is_dir() else child.unlink()
 
 def save_db(path, db):
     for file, content in db.items():
-        file_path = doc_path / (file + ".html")
+        file_path = path / (file + ".html")
         file_path.parent.mkdir(parents=True, exist_ok=True)
 
         content = re.sub('((href=")(?!http)[^"#]+)', '\\1.html', content)
         file_path.write_text(content)
 
 def save_index(path, index):
-    index = [{"name": e["name"], "path": e["path"]} for e in index["entries"]]
-    json.dump(index, path.open("w"))
+    index = [{ "name": e["name"], "path": e["path"] } for e in index["entries"]]
+    with path.open("w") as fp: json.dump(index, fp)
 
 ####################
 root = Path("html")
 if not root.exists():
     raise RuntimeError("Must be run from within the root directory")
 
-with open("update.conf") as file:
-    active = [line.rstrip() for line in file]
+with open("update.conf") as fp:
+    wanted = [ ln.rstrip() for ln in fp ]
 
 opener = request.build_opener()
 opener.addheaders = [("User-agent", "Mozilla/5.0")]
 request.install_opener(opener)
 
-index_path = root / index_name
-index = download1(docs_url, index_path, "Overall index")
+docs = download(docs_url, "Document index")
 
-for doc in index:
-    doc_mtime = str(doc["mtime"])
-    doc_slug = doc["slug"]
-    doc_path = root / doc["type"] / doc_slug
+for doc in docs:
+    mtime = str(doc["mtime"])
+    name = doc["slug"]
+    path = root / doc["type"] / name
 
-    if not doc_slug in active: continue
+    if not name in wanted: continue
 
-    mtime_path = doc_path / mtime_name
-    mtime = mtime_path.read_text() if mtime_path.exists() else ""
+    mtime_path = path / "mtime"
+    old_mtime = mtime_path.read_text() if mtime_path.exists() else ""
 
-    if doc_mtime != mtime:
-        doc_index, doc_db = download2(doc_path, doc_slug)
+    if mtime != old_mtime:
+        index = download(doc_templ_url.format(name, "index.json"), name + " index")
+        db = download(doc_templ_url.format(name, "db.json"), name + " db")
 
-        clean(doc_path)
+        path.mkdir(parents=True, exist_ok=True)
+        purge(path)
 
-        save_db(doc_path, doc_db)
-        save_index(doc_path / index_name, doc_index)
+        save_db(path, db)
+        save_index(path / "index.json", index)
 
-        mtime_path.write_text(doc_mtime)
+        mtime_path.write_text(mtime)
 
 print("DONE")
