36fcd1b47e9b6efdd0fd5b2b40d763df77c65954
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2023-12-09 21:27:16 -0500

    Unpack db and process index

--- update.py
+++ update.py
@@ -2,9 +2,18 @@
 
 import json
 from pathlib import Path
+import re
+import shutil
 import urllib.request as request
 
-def download(url, path, info):
+docs_url = "https://devdocs.io/docs.json"
+doc_prefix_url = "https://documents.devdocs.io"
+
+index_name = "index.json"
+db_name = "db.json"
+mtime_name = "mtime"
+
+def download1(url, path, info):
     def spin():
         while True:
             for c in "||//--\\\\": yield c
@@ -19,22 +28,44 @@ def download(url, path, info):
 
     return json.load(open(path))
 
-index_name = "index.json"
-db_name = "db.json"
-mtime_name = "mtime"
+def download2(path, name):
+    path.mkdir(parents=True, exist_ok=True)
 
-with open("update.conf") as file:
-    active = [line.rstrip() for line in file]
+    index = download1(doc_prefix_url+"/"+name+"/"+index_name, path / index_name, name+" index")
+    db = download1(doc_prefix_url+"/"+name+"/"+db_name, path / db_name, name+" db")
 
+    return index, db
+
+def clean(path):
+    for child in path.iterdir():
+        shutil.rmtree(child) if child.is_dir() else child.unlink()
+
+def save_db(path, db):
+    for file, content in db.items():
+        file_path = doc_path / (file + ".html")
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        content = re.sub('((href=")(?!http)[^"#]+)', '\\1.html', content)
+        file_path.write_text(content)
+
+def save_index(path, index):
+    index = [{"name": e["name"], "path": e["path"]} for e in index["entries"]]
+    json.dump(index, path.open("w"))
+
+####################
 root = Path("html")
 if not root.exists():
     raise RuntimeError("Must be run from within the root directory")
+
+with open("update.conf") as file:
+    active = [line.rstrip() for line in file]
 
 opener = request.build_opener()
 opener.addheaders = [("User-agent", "Mozilla/5.0")]
 request.install_opener(opener)
 
-index = download("https://devdocs.io/docs.json", root / index_name, "Doc index")
+index_path = root / index_name
+index = download1(docs_url, index_path, "Overall index")
 
 for doc in index:
     doc_mtime = str(doc["mtime"])
@@ -47,11 +78,12 @@ for doc in index:
     mtime = mtime_path.read_text() if mtime_path.exists() else ""
 
     if doc_mtime != mtime:
-        doc_path.mkdir(parents=True, exist_ok=True)
+        doc_index, doc_db = download2(doc_path, doc_slug)
 
-        url = "https://documents.devdocs.io/" + doc_slug + "/"
-        doc_index = download(url + index_name, doc_path / index_name, doc_slug + "/index")
-        doc_db = download(url + db_name, doc_path / db_name, doc_slug + "/db")
+        clean(doc_path)
+
+        save_db(doc_path, doc_db)
+        save_index(doc_path / index_name, doc_index)
 
         mtime_path.write_text(doc_mtime)
 
