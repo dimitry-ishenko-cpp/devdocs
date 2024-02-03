91bbc279247cac9ce8e2f27397ab6a8ae3448276
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-02-02 21:40:10 -0500

    debian: install latest versions only

--- debian/update.sh
+++ debian/update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 dir=$(dirname $(dirname $(realpath "$0")))
 cd "$dir"
@@ -6,13 +7,30 @@ cd "$dir"
 cp bin/devmanage.in ./devmanage
 sed -i -e "s|@DEVDOCS_INSTALL_DATADIR@|$dir|g" ./devmanage
 
-mkdir -p html
-(cd html && ../devmanage --all | tail -n+2 | xargs mkdir -p)
+all=$(./devmanage --all | tail -n+2 | sort -rV)
+
+new=
+declare -A had
+for doc in $all; do
+    mod=${doc//[~0-9.]*/}
+    [[ -n "${had[$mod]}" ]] || had[$mod]=1 new+=" $doc"
+done
+
+cd html
+del=$(echo $new $new $(ls -1) | tr ' ' '\n' | sort -V | uniq -u)
+if [[ -n "$del" ]]; then
+    echo ABOUT TO DELETE ...
+    echo $del
+    sleep 3
+    rm -r $del
+fi
+echo $new | xargs mkdir -p
+cd ..
 
 ./devmanage --update
 
 cp debian/control.in debian/control
-for doc in $(cd html && ls -1); do
+for doc in $new; do
     deb="devdocs-data-${doc//[_~]/-}"
     cat <<EOF >> debian/control
 
