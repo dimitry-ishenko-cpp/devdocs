627aa824b6aac2e895b6092b029cdfa87f7d20e9
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-10-06 19:06:12 -0400

    hooks.py: add line breaks

--- elinks/hooks.py
+++ elinks/hooks.py
@@ -1,18 +1,26 @@
 from io import StringIO
 from lxml import html as lh
+from lxml import etree as lt
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
 
+formatter = HtmlFormatter(full=False, noclasses=True, nobackground=True)
+
 def pre_format_html_hook(url, html):
     tree = lh.parse(StringIO(html))
 
+    # add border=1 to <table> elements
     for table in tree.findall(".//table"):
         if not table.get("border"):
             table.set("border", "1")
 
-    formatter = HtmlFormatter(full=False, noclasses=True, nobackground=True)
+    # add <br> inside <span class="t-lines"> elements
+    for elem in tree.xpath('.//span[@class="t-lines"]'):
+        for index in reversed(range(1, len(elem.getchildren()))):
+            elem.insert(index, lt.Element("br"))
 
+    # highlight <pre data-language="..."> elements
     for code in tree.findall(".//pre"):
         if lang := code.get("data-language"):
             try:
